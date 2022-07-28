155af8a33f2cc37b7636790c7f7e1b71b426763a
Vladi Yudi <vladiyudi@Vladis-MacBook-Air.local>
2022-07-28 11:39:54 +0300

    Firestore servers + error and spinners

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Home from './Routes/Home.js'
-import { BrowserRouter, Routes,
-  Route } from "react-router-dom";
+import Home from "./Routes/Home.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./Routes/Profile";
-import { useState, useEffect} from "react";
-import { collection, getDocs, addDoc } from "firebase/firestore";
-
-import {db} from "./fire.js"
-import Tweet from "./Components/Tweet";
-
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [userName, setUserName] = useState(  localStorage.getItem("Profile") ? JSON.parse(localStorage.getItem("Profile")):'Vladi')
+  const [userName, setUserName] = useState(
+    localStorage.getItem("Profile")
+      ? JSON.parse(localStorage.getItem("Profile"))
+      : "Vladi"
+  );
   
-  const handleProfile = (input)=>{
-    localStorage.setItem("Profile", JSON.stringify(input))
-    setUserName(input)
-  }
+  const handleProfile = (input) => {
+    localStorage.setItem("Profile", JSON.stringify(input));
+    setUserName(input);
+  };
 
-  const colRef = collection(db, 'Tweets')
-  const [fireTweets, setFireTweets] = useState([])
-
-  const getSnapshot = async ()=>{
-    try {
-      const tw = []
-   const snapshot = await getDocs(colRef)
-   snapshot.forEach((doc)=>{
-    tw.push({...doc.data(), id: doc.id})
-   })
-   tw.sort((a,b)=>{return new Date(b.date) - new Date(a.date)})
-  setFireTweets(tw.splice(0, 50))
-  }
-   catch (err){ console.error(err.message)}
-}
-
-useEffect(()=>{
-  getSnapshot()
-}, [])
-
-
- 
   return (
     <div className="App">
-  <BrowserRouter>
-  <Navbar/>
-  <Routes>
-  <Route path="/" element= { <Home user={userName} colRef={colRef} getSnapshot={getSnapshot} fireTweets={fireTweets}/> }/>
-  <Route path="/Profile" element={ <Profile handleProfile={handleProfile} userName={userName}/>}/>
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home user={userName} />} />
+          <Route
+            path="/Profile"
+            element={
+              <Profile handleProfile={handleProfile} userName={userName} />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -4,76 +4,58 @@ import TweetList from "../Components/TweetList";
 import { useEffect, useState, createContext } from "react";
 import { nanoid } from "nanoid";
 import Spinner from "react-bootstrap/Spinner";
-import { addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+
+import { db } from "../fire.js";
 
 export const TweetContext = createContext([]);
 
-export default function Home({ user, colRef, getSnapshot, fireTweets }) {
-  const [serverTweets, setServerTweets] = useState([]);
+export default function Home({ user }) {
+  const [fireTweets, setFireTweets] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const colRef = collection(db, "Tweets");
 
-  // const postToServer = async (tweet) => {
-  //   try {
-  //     setLoading(true);
-  //     const res = await fetch(
-  //       "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet",
-  //       {
-  //         method: "POST",
-  //         headers: { "Content-type": "application/json;charset=UTF-8" },
-  //         body: JSON.stringify({
-  //           content: tweet.content,
-  //           userName: tweet.userName,
-  //           date: tweet.date,
-  //           id: tweet.id,
-  //         }),
-  //       }
-  //     );
-  //     const data = await res.json();
-  //     if (!res.ok) {
-  //       setError("Error: " + data.message);
-  //       setTimeout(() => {
-  //         setError("");
-  //       }, 3000);
-  //       throw new Error(data.message);
-  //     }
-  //     setLoading(false);
-  //     setServerTweets([tweet, ...serverTweets]);
-  //   } catch (err) {
-  //     console.error(err);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+  const getSnapshot = async () => {
+    try {
+      setLoading(true);
+      const tw = [];
+      const snapshot = await getDocs(colRef);
+      snapshot.forEach((doc) => {
+        tw.push({ ...doc.data(), id: doc.id });
+      });
+      tw.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      });
+      setFireTweets(tw.splice(0, 50));
+      setLoading(false);
+    } catch (err) {
+      console.error(err.message);
+      setError("Error: " + err.message);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  };
 
-  // const getFromServer = async () => {
-  //   try {
-  //     setLoading(true);
-  //     const res = await fetch(
-  //       "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet"
-  //     );
-  //     if (!res.ok) throw new Error((err) => err.message());
-  //     const data = await res.json();
-  //     setServerTweets(data.tweets);
-  //     setLoading(false);
-  //     return data.tweets;
-  //   } catch (err) {
-  //     setError("Error: " + err);
-  //     setTimeout(() => {
-  //       setError("");
-  //     }, 3000);
-  //     console.log("error ", err);
-  //   }
-  // };
+  const addToFirestore = async (tweet) => {
+    try {
+      setLoading(true);
+      const res = await addDoc(colRef, tweet);
+      getSnapshot();
+      setLoading(false);
+    } catch (err) {
+      setError("Error: " + err.message);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+      console.error(err);
+    }
+  };
 
-  // useEffect(() => {
-  //   getFromServer();
-  //   const interval = setInterval(() => {
-  //     getFromServer();
-  //   }, 10000);
-  //   return () => clearInterval(interval);
-  // }, []);
-  
+  useEffect(() => {
+    getSnapshot();
+  }, []);
 
   const handleButton = (input) => {
     const tweet = {
@@ -83,14 +65,8 @@ export default function Home({ user, colRef, getSnapshot, fireTweets }) {
       date: new Date().toISOString(),
     };
     // postToServer(tweet);
-    addToFirestore(tweet)
-
+    addToFirestore(tweet);
   };
-
-  const addToFirestore = async (tweet)=>{
-    const res = await addDoc(colRef, tweet)
-    getSnapshot()
-  }
 
   return (
     <div>
@@ -109,3 +85,66 @@ export default function Home({ user, colRef, getSnapshot, fireTweets }) {
     </div>
   );
 }
+
+// const [serverTweets, setServerTweets] = useState([])
+
+// const postToServer = async (tweet) => {
+//   try {
+//     setLoading(true);
+//     const res = await fetch(
+//       "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet",
+//       {
+//         method: "POST",
+//         headers: { "Content-type": "application/json;charset=UTF-8" },
+//         body: JSON.stringify({
+//           content: tweet.content,
+//           userName: tweet.userName,
+//           date: tweet.date,
+//           id: tweet.id,
+//         }),
+//       }
+//     );
+//     const data = await res.json();
+//     if (!res.ok) {
+//       setError("Error: " + data.message);
+//       setTimeout(() => {
+//         setError("");
+//       }, 3000);
+//       throw new Error(data.message);
+//     }
+//     setLoading(false);
+//     setServerTweets([tweet, ...serverTweets]);
+//   } catch (err) {
+//     console.error(err);
+//   } finally {
+//     setLoading(false);
+//   }
+// };
+
+// const getFromServer = async () => {
+//   try {
+//     setLoading(true);
+//     const res = await fetch(
+//       "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet"
+//     );
+//     if (!res.ok) throw new Error((err) => err.message());
+//     const data = await res.json();
+//     setServerTweets(data.tweets);
+//     setLoading(false);
+//     return data.tweets;
+//   } catch (err) {
+//     setError("Error: " + err);
+//     setTimeout(() => {
+//       setError("");
+//     }, 3000);
+//     console.log("error ", err);
+//   }
+// };
+
+// useEffect(() => {
+//   getFromServer();
+//   const interval = setInterval(() => {
+//     getFromServer();
+//   }, 10000);
+//   return () => clearInterval(interval);
+// }, []);
